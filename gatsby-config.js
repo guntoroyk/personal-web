fcdbb4f22815ab429a7ca13d99fe6a5290d38d59
guntoroyk <guntoro.gyk@gmail.com>
2020-05-11 20:20:57 +0700

    update portfolio page, delete default blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,12 +17,12 @@ module.exports = {
             slug: `/blog`,
           },
           {
-            title: `About`,
-            slug: `/about`,
-          },
-          {
             title: `Portfolio`,
             slug: `/portfolio`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
           },
         ],
         externalLinks: [
@@ -59,16 +59,16 @@ module.exports = {
         theme_color: `#6B46C1`,
         display: `standalone`,
         icons: [
-          // {
-          //   src: `/android-chrome-192x192.png`,
-          //   sizes: `192x192`,
-          //   type: `image/png`,
-          // },
-          // {
-          //   src: `/android-chrome-512x512.png`,
-          //   sizes: `512x512`,
-          //   type: `image/png`,
-          // },
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
         ],
       },
     },
