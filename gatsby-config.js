3e3b8b02b175e1a76a102c296374d1dc3058cea7
Guntoro Yudhy Kusuma <guntoro.gyk@gmail.com>
2020-10-02 22:57:50 +0700

    update sitemap meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,9 +51,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
-        short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        name: `Guntoro Yudhy Kusuma - Student, Freelancer, Full Stack Engineer`,
+        short_name: `guntoro-yudhy-kusuma`,
+        description: `Hi, I'm Guntoro! I'm a student of UGM majoring in Internet Engineering Technology. Besides as student, I also work as freelancer and partimer in software engineer field. I've strong passion in backend and frontend stack. Also, I'm interested in learning and exploring new things about tech such cloud computing, computer network, and devops.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
